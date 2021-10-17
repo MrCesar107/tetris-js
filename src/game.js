1be48e5080a7237d307bb10fdd0c3c4d6a4238f0
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-10-17 01:28:09 -0500

    Generate pieces randomly

--- src/game.js
+++ src/game.js
@@ -1,3 +1,4 @@
+import { randomInRange } from "./utils/random_in_range.js";
 import Piece from "./entities/piece.js";
 
 export default class Game {
@@ -6,6 +7,7 @@ export default class Game {
     this.ctx = this.canvas.getContext("2d");
     this.pieces = [];
     this.animationId = null;
+    this.currentPiece = null;
   }
 
   initGame() {
@@ -14,20 +16,34 @@ export default class Game {
   }
 
   update() {
-    this.pieces.push(new Piece(100, 0, 1, "#ff0000", canvas.height));
+    this.generateCurrentPiece();
     this.animate();
 
     addEventListener("keydown", (event) => {
-      if (event.code == "ArrowLeft") this.pieces[0].move("left");
-      if (event.code == "ArrowRight") this.pieces[0].move("right");
-      if (event.code == "ArrowDown") this.pieces[0].move("down");
+      if (event.code == "ArrowLeft") this.currentPiece.move("left");
+      if (event.code == "ArrowRight") this.currentPiece.move("right");
+      if (event.code == "ArrowDown") this.currentPiece.move("down");
     });
   }
 
   animate() {
     this.animationId = requestAnimationFrame(this.animate.bind(this));
     this.clearCanvas();
-    this.pieces[0].update(this.ctx);
+    this.currentPiece.update(this.ctx);
+    this.pieces.forEach((piece) => piece.update(this.ctx));
+    this.pushCurrentPiece();
+  }
+
+  generateCurrentPiece() {
+    const pieceType = randomInRange(1, 7);
+    this.currentPiece = new Piece(100, 0, pieceType, "#ff0000", canvas.height);
+  }
+
+  pushCurrentPiece() {
+    if (this.currentPiece.isCollidingBottom) {
+      this.pieces.push(this.currentPiece);
+      this.generateCurrentPiece();
+    }
   }
 
   resizeCanvas() {
